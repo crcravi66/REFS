11debd3e02458dd9853e8b2455ba0bcd5d0315fa
arun <crcravi66@gmail.com>
2024-06-15 13:32:29 +0530

    compelted for refs react project

--- REFS/src/components/ResultModal.jsx
+++ REFS/src/components/ResultModal.jsx
@@ -1,11 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
-    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -16,15 +18,18 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     })
 
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal"  >
             {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2> Your Score = {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You Stopped the timer with <strong>{formattedRemainingTime} second left.</strong></p>
             <form method="dialog" onSubmit={onReset} >
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
+
     )
 })
 
